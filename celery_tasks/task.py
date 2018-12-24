55e109871efed67d6cf6efd565491a9db5f7cefd
muming <2859595067@qq.com>
2018-12-24 22:14:54 +0800

    异步邮件

--- celery_tasks/task.py
+++ celery_tasks/task.py
@@ -13,7 +13,7 @@ def send_msg(email, randomCode):
     # 第五个参数为：可以将html标签翻译如a标签中如果有href属性他的内容就可以被点击。
     # 注意：第二个参数和第五个参数不能同时存在，如果同时存在第五个参数会覆盖掉第二个参数中的内容。
     send_mail(
-        "验证您的用户信息", '您已经注册账户成功，现在需要您访问如下链接：http://localhost:8000/check?code=' + randomCode,
+        "验证您的用户信息", '您已经注册账户成功，现在需要您访问如下链接便可以激活您的账户：http://localhost:8000/check?code=' + randomCode,
         settings.EMAIL_HOST_USER, [email, ],
-        html_message='您已经注册账户成功，现在需要您访问如下链接：http://localhost:8000/check?code=' + randomCode,
+        html_message='您已经注册账户成功，现在需要您访问如下链接便可以激活您的账户：http://localhost:8000/check?code=' + randomCode,
     )
